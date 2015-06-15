489b89fcdf5c7b618d214e8bb7fb236026744fbd
Tiffany Chen <tchen@thoughtworks.com>
2015-06-15 14:06:32 -0500

    [Tiffany, Tulara] Deletes database configuration

--- db/create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function createdb() {
-  DATABASE=$1
-  PGPASSWORD=postgres psql -U postgres -h localhost -c "drop database ${DATABASE}"
-  PGPASSWORD=postgres psql -U postgres -h localhost -c "create database ${DATABASE}"
-}
-
-createdb "simpleSpring"
