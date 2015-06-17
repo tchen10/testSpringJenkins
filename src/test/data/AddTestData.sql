cd14e583b725b9758b56ffdd35232d9655c2e9ee
sebradloff <sradloff23@gmail.com>
2015-06-17 15:55:42 -0500

    [Sebastian] added load test data to the build file.

---
+++ src/test/data/AddTestData.sql
@@ -0,0 +1,4 @@
+INSERT INTO project (name) VALUES('SWA');
+INSERT INTO project (name) VALUES('CATALINA');
+INSERT INTO project (name) VALUES('KROGER');
+INSERT INTO project (name) VALUES('Barclays');
